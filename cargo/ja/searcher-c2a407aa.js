2b6e6ad5c51896a0ecf1b5d5106f4a7fac33204f
partrita <partrita@users.noreply.github.com>
2026-04-12 05:33:09 +0000

    deploy: ae365971357b14a68b9959fa82409f5a642c5e92

--- cargo/ja/searcher-c2a407aa.js
+++ cargo/ja/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-a4ba3e52.js',
+                path_to_root + 'searchindex-c7027f69.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
